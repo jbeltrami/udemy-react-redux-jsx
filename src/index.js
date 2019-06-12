fd49ee7e048c31f75767f8bfb93bd3cae359c47d
jbeltrami <joao_beltrami@hotmail.com>
2019-06-12 10:15:23 -0400

    Start jsx lecture from Stephen Grider's Udemy course
    Run create-react-app
    Run yarn eject
    Install webos eslint configuration
    Import react, create component and render it to DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
+// Import React and ReactDOM libs
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// Create a react component
+const App = () => <div>Hi there!</div>;
+
+// Render created component to the DOM.
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
