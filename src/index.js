7e6fa5590411495bade31560a2bdeee3fdf3b525
jbeltrami <joao_beltrami@hotmail.com>
2019-07-09 15:26:42 -0400

    JSX lecture from Udemy - Modern React Redux course

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
-const App = () => <div>Hi there!</div>;
+const App = () => {
+  const buttonText = 'Click Me!';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+        <input id="name" type="text" />
+      </label>
+
+      <button type="button" style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
 // Render created component to the DOM.
 ReactDOM.render(<App />, document.getElementById('root'));
